d9c271709b11e967f008c221834df0cd43b24248
Lukas <Lukas@DESKTOP-E675PCP>
2017-10-14 10:00:07 +0200

    Add project files.

---
+++ Dojo1/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo1
+{
+    class Stack<T>
+    {
+        public StackElement<T> first;
+
+        public T Peek()
+        {
+            if (first != null)
+            {
+                return first.value;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public void Push(T element)
+        {
+            if (first != null)
+            {
+                StackElement<T> temp = new StackElement<T>() {value = element, nextElement=first };
+                first = temp;
+            }
+            else
+            {
+                first = new StackElement<T>()
+                {
+                    value = element,
+                    nextElement = null
+                };
+            }
+        }
+
+        public T Pop()
+        {
+            if (first != null)
+            {
+                T temp = first.value;
+                first = first.nextElement;
+                return temp;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+    }
+}
---
+++ Dojo1/StackElement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dojo1
+{
+    class StackElement<T>
+    {
+        public T value { get; set; } //Stores the value of the staack entry
+        public StackElement<T> nextElement { get; set; }
+    }
+}
